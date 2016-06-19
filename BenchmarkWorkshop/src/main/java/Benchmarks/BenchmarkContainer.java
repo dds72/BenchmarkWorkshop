472edd071e31cf70fb379b88d21a29089c77d694
dds72 <ddstmn@gmail.com>
2016-06-19 22:33:10 +0300

    Benchmarks container provided.

---
+++ BenchmarkWorkshop/src/main/java/Benchmarks/BenchmarkContainer.java
@@ -0,0 +1,18 @@
+package Benchmarks;
+
+/**
+ * Created by dds on 19.06.2016.
+ */
+public class BenchmarkContainer {
+    public void Setup() {
+
+    }
+
+    public void LockBenchmark() {
+
+    }
+
+    public void MutexBenchmark() {
+
+    }
+}
